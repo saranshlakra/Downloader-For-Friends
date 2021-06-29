2caa3e22976217d7fe6ad453619c581cfaa8f998
saranshlakra <saranshlakra4@gmail.com>
2021-06-29 16:22:30 +0530

    password updated

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// function password() {
-//   var passwrd = prompt("Enter password to open");
+function password() {
+  var passwrd = prompt("Enter password to open");
 
-//   if (passwrd.toString() == "password@8860") {
-//     document.querySelector(".password-2").style.display = "block";
-//   } else {
-//     alert("Incorrect Password! Please try again");
-//     document.querySelector(".password-2").style.display = "none";
-//   }
-// }
+  if (passwrd.toString() == "password@8860") {
+    document.querySelector(".password-2").style.display = "block";
+  } else {
+    alert("Incorrect Password! Please try again");
+    document.querySelector(".password-2").style.display = "none";
+  }
+}
